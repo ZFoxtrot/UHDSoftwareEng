f25dcb041a7af80b38991b4b47bc1b5fbd80244c
Anthony Pham <apham2021114@gmail.com>
2019-10-16 17:11:44 -0500

    Registered models from models.py. Probably won't be needing all of them here.

--- GradeManagement/admin.py
+++ GradeManagement/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Course, Assignment, Semester, AssignmentGrade
 
 # Register your models here.
+admin.site.register(Course)
+admin.site.register(Assignment)
+admin.site.register(Semester)
+admin.site.register(AssignmentGrade)
