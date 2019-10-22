d695a7ce4298f4f811edbac8f2a803b6c540b455
Anthony Pham <apham2021114@gmail.com>
2019-10-21 20:55:33 -0500

    Imported Enrollment. Registered Enrollment model. (Feel free to remove.)

--- GradeManagement/admin.py
+++ GradeManagement/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Course, Assignment, Semester, AssignmentGrade
+from .models import User, Course, Assignment, Semester, AssignmentGrade, Enrollment
 
 # Register your models here.
 admin.site.register(Course)
 admin.site.register(Assignment)
 admin.site.register(Semester)
 admin.site.register(AssignmentGrade)
+admin.site.register(Enrollment)
