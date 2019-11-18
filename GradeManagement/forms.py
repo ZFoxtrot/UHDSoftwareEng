49d0663899be293acf1f3c4b8daf007517acf826
Anthony Pham <apham2021114@gmail.com>
2019-11-18 13:01:08 -0600

    Included Semester and Course forms

--- GradeManagement/forms.py
+++ GradeManagement/forms.py
@@ -1,10 +1,24 @@
 from django import forms
 
-from .models import Assignment
+from .models import Assignment, Semester, Course
 
 
 class AssignmentForm(forms.ModelForm):
 
     class Meta:
         model = Assignment
-        fields = ('Title', 'Due_date', 'Description',)
+        fields = ('Title', 'Due_date', 'Description', 'Course',)
+
+
+class SemesterForm(forms.ModelForm):
+
+    class Meta:
+        model = Semester
+        fields = ('Title', 'StartDate', 'EndDate', 'Active')
+
+
+class CourseForm(forms.ModelForm):
+
+    class Meta:
+        model = Course
+        fields = ('Name', 'Teacher', 'SemesterOfCourse')
