8e43f6616ee00973914d810215b1ea51e88a6b1c
Anthony Pham <apham2021114@gmail.com>
2019-11-20 21:53:07 -0600

    Added AddUserForm and EditUserForm

--- GradeManagement/forms.py
+++ GradeManagement/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .models import Assignment, Semester, Course
 
@@ -22,3 +24,39 @@ class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
         fields = ('Name', 'Teacher', 'SemesterOfCourse')
+
+
+# Form for creating additional users
+# Inherits django's user creation form
+class AddUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'password1',
+            'password2'
+        )
+
+    def save(self, commit=True):
+        user = super(AddUserForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_date['last_name']
+        user.email = self.cleaned_date['email']
+        user.save()
+        return user
+
+
+class EditUserForm(UserChangeForm):
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'email',
+        )
